bc022366d638d20cdb5e0bad1255f447e01dd479
Yasiru Lakintha <yasiru.upwork@gmail.com>
2022-10-18 19:08:32 +0530

    basics done

---
+++ index.js
@@ -0,0 +1,107 @@
+const el = document.getElementById("line3")
+
+setTimeout(() => {
+  el.style.display = 'block';
+  el.innerHTML = '[Connecting To the Database]';
+}, 6800);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database.]';
+}, 7200);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database..]';
+}, 7400);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database...]';
+}, 7600);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database.]';
+}, 7800);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database..]';
+}, 8000);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database...]';
+}, 8200);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database.]';
+}, 8400);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database..]';
+}, 8600);
+
+setTimeout(() => {
+    el.innerHTML = '[Connecting To the Database...]';
+}, 8800);
+
+setTimeout(() => {
+    el.innerHTML = '[Database Connected]';
+  }, 9000);
+
+
+const el3 = document.getElementById("line-dash")
+
+setTimeout(() => {
+      el3.style.display = 'block';
+    }, 11600);
+
+const el2 = document.getElementById("line5")
+
+setTimeout(() => {
+  el2.style.display = 'block';
+  el2.innerHTML = '[Loading Event Details]';
+}, 11800);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details.]';
+}, 12000);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details..]';
+}, 12200);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details...]';
+}, 12400);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details.]';
+}, 12600);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details..]';
+}, 12800);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details...]';
+}, 13000);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details.]';
+}, 13200);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details..]';
+}, 13400);
+
+setTimeout(() => {
+    el2.innerHTML = '[Loading Event Details...]';
+}, 13600);
+
+setTimeout(() => {
+    el2.innerHTML = '[Event Details]';
+  }, 13800);
+
+
+const el4 = document.getElementById("line-dash2")
+
+setTimeout(() => {
+        el4.style.display = 'block';
+      }, 20800);
